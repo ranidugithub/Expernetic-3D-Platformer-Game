1c210bfd1f485cf1061d7bb9244b4c74430f7189
Hyphen <34578972+ranidugithub@users.noreply.github.com>
2024-05-24 19:43:38 +0530

    add environmental lighting

--- Expernetic 3D Platform Game/Assets/Scripts/CoinScore.cs
+++ Expernetic 3D Platform Game/Assets/Scripts/CoinScore.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CoinScore : MonoBehaviour
 {
+    public AudioSource coinSound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
           ScoreManager.sManager.IncreaseScore(10);
+          coinSound.Play();
           Destroy(this.gameObject);
         }
     }
